518733837d3d21c7b63d63eaca00d11e9895d349
nahid_dev <nahid.dev19@gmail.com>
2025-08-19 09:02:08 +0600

    auth page 2

--- my-app/src/app/(auth)/login/page.tsx
+++ my-app/src/app/(auth)/login/page.tsx
@@ -1,7 +1,10 @@
-import Login from "@/components/pages/login";
-import React from "react";
+import Login from '@/components/pages/login';
+import db from '@/lib/prisma';
+import React from 'react';
 
-export default function page() {
+export default async function page() {
+  const users = await db.user.findFirst();
+  console.log(users);
   return (
     <div className="flex h-screen items-center justify-center">
       <Login />
