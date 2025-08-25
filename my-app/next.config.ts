1062b01134cb3467555424f23e6d97d155122bff
nahid_dev <nahid.dev19@gmail.com>
2025-08-25 08:53:32 +0600

    issue fix

--- my-app/next.config.ts
+++ my-app/next.config.ts
@@ -1,15 +1,7 @@
-const path = require("path");
-
-module.exports = {
-  webpack: (config) => {
-    // Prevent webpack from trying to scan Windows system dirs
-    config.watchOptions = {
-      ignored: [
-        '**/node_modules',
-        '**/.git',
-        path.resolve('C:/Users/Imtiaz/Application Data')
-      ],
-    };
-    return config;
-  },
-};
+import type { NextConfig } from 'next'
+ 
+const nextConfig: NextConfig = {
+  // ...
+}
+ 
+export default nextConfig
